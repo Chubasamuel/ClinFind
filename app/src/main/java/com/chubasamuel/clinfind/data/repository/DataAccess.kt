2d9db9ee62d9ce5a3e63f13aa5f2dc54778788b4
Chuba Samuel <chubasamuel@gmail.com>
2023-12-14 15:48:24 +0100

    Added retries to API calls

--- app/src/main/java/com/chubasamuel/clinfind/data/repository/DataAccess.kt
+++ app/src/main/java/com/chubasamuel/clinfind/data/repository/DataAccess.kt
@@ -5,6 +5,7 @@ import com.chubasamuel.clinfind.data.remote.Resource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.emitAll
@@ -16,20 +17,22 @@ fun <T> getData( apiReq: suspend () -> Resource<T>,
     callbackFlow {
         trySend(Resource.loading())
         CoroutineScope(Dispatchers.IO).launch {
-        val response = apiReq.invoke()
+        var retries = 0L
+        var response = apiReq.invoke()
+        while(retries<7&&response.status!=Resource.Status.SUCCESS){
+            retries+=1
+            delay(1000*retries)
+            response = apiReq.invoke()
+        }
+
         if (response.status == Resource.Status.SUCCESS) {
             Log.i("DCOR DEBUG","printing network res")
             Log.i("DCOR DEBUG","Network Data: ${response.data}")
             response.data?.let { trySend(Resource.success(it)); saveRes(it) }
 
         } else if (response.status == Resource.Status.ERROR) {
-            trySend(Resource.error(response.message?:"Unidentified error occurred while fetching data over the internet"))
+            trySend(Resource.error("Error fetching data: "+ (response.message?:"Unidentified error occurred while fetching data over the internet")))
         }}
-        /*val source = dbQuery.invoke()
-        source.collect{data->
-            Log.i("DCOR DEBUG","printing res")
-            Log.i("DCOR DEBUG","db res: $data")
-            trySend(Resource.success(data))}*/
         awaitClose{}
     }
 fun <T> getDataV2(dbQuery:  () -> Flow<T>): Flow<Resource<T>> =
